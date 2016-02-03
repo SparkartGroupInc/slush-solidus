5b86fe84d5d8cb0f382546225bcde332be66d4e7
Josiah Sprague <josiah.sprague@gmail.com>
2016-02-03 16:52:48 -0300

    clean up generator

--- slushfile.js
+++ slushfile.js
@@ -4,14 +4,12 @@ var argv = require('minimist')(process.argv.slice(2));
 
 var options = {
   siteName: argv.name || 'New Solidus Site',
-  siteSlug: argv.name.toLowerCase().replace(' ', '-') || 'new-solidus-site'
+  siteSlug: argv.name.toLowerCase().replace(/ /g, '-') || 'new-solidus-site'
 };
 
 gulp.task('default', function(done) {
-  console.log('Args: ', gulp.args);
-  console.log('Argv: ', argv);
   gulp.src(__dirname + '/templates/site/**/{*,.*}')
     .pipe(template(options))
-    .pipe(gulp.dest('./' + options.siteName));
+    .pipe(gulp.dest('./' + options.siteSlug));
 });
 
