e2615c296117ec227b67052383b5f24deda685b0
Josiah Sprague <josiah.sprague@gmail.com>
2016-02-03 15:02:43 -0300

    add dotfiles to glob

--- slushfile.js
+++ slushfile.js
@@ -3,13 +3,13 @@ var template = require('gulp-template');
 var argv = require('minimist')(process.argv.slice(2));
 
 var options = {
-  siteName: argv.name || argv.n
+  siteName: argv.name || argv.n || 'solidus-site'
 };
 
 gulp.task('default', function(done) {
   console.log('Args: ', gulp.args);
   console.log('Argv: ', argv);
-  gulp.src(__dirname + '/templates/site/**')
+  gulp.src(__dirname + '/templates/site/**/{*,.*}')
     .pipe(template(options))
     .pipe(gulp.dest('./' + options.siteName));
 });
