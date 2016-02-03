33bd31088c9b5ec8f87ce3b50e0c4d362c4a7a7c
Josiah Sprague <josiah.sprague@gmail.com>
2016-02-03 16:42:19 -0300

    update template

--- templates/site/assets/scripts/index.js
+++ templates/site/assets/scripts/index.js
@@ -1,7 +1,7 @@
 /*
 All your JS starts here!
 Use browserify (http://browserify.org/) to include more JS files.
-You can use `gulp build` to write the compiled file to /compiled/index.js
+You can use `gulp build` to write the compiled file to /assets/compiled/index.js
 */
 
 // Remove everything below this line to get started
